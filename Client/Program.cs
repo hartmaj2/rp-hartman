2b6f6ab4286117c746dd7b76ae8b435f4c7aa642
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-10-19 11:30:09 +0200

    Remove unnecessary service

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,7 +8,6 @@ builder.RootComponents.Add<App>("#app"); // adds the App.razor component as a ro
 // Scoped services live throughout one user connection (a new instance of the service is created only when the page is reloaded)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<AllergenService>(); // add my allergen service so I can pass list of all possible allergens to components that inject this service
-builder.Services.AddScoped<MealService>();  // similar to allergen service above
 builder.Services.AddScoped<ClientModeService>(); // tracks changes between bug reporting and normal mode
 builder.Services.AddBlazorBootstrap(); // add service for BlazorBootstrap needed for modals and dropdowns
 
