6a058d7ba2643820518430850d141b07efd69ef5
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-10-20 10:14:35 +0200

    Add event triggering when user confirms element selection and add comments

--- Client/Services/ClientModeService.cs
+++ Client/Services/ClientModeService.cs
@@ -1,4 +1,6 @@
-// Tracks the current mode of the client web page, the page can be in modes: 1. Normal 2. BugReporting
+/// <summary>
+/// Tracks the current mode of the client web page, the page can be in modes: 1. Normal 2. BugReporting
+/// </summary>
 public class ClientModeService
 {
     private ClientMode _currentMode;
@@ -7,21 +9,38 @@ public class ClientModeService
 
     public event Action? OnModeChangedEvent; // field is nullable, if no subsribers -> null
 
+    /// <summary>
+    /// Triggers when user confirms selection of suspicious elements
+    /// </summary>
+    public event Action? OnBugSelectionConfirmedEvent;
+
     public ClientModeService()
     {
         _currentMode = ClientMode.Normal;
     }
 
-    // Flips the mode to the other one
+    /// <summary>
+    /// Flips the mode to the other one
+    /// </summary>
     public void ToggleMode()
     {
         _currentMode = _currentMode == ClientMode.Normal ? ClientMode.BugReporting : ClientMode.Normal;
         OnModeChangedEvent?.Invoke(); // only invoke when this event has subscribers
     }
+
+    /// <summary>
+    /// Triggers the event that will notify the page that the user wants to check the selection of suspicious elements
+    /// </summary>
+    public void ConfirmBugSelection()
+    {
+        OnBugSelectionConfirmedEvent?.Invoke();
+    }
 }
 
 
-// Enumeration for all the possible web page modes
+/// <summary>
+/// Enumeration for all the possible web page modes
+/// </summary>
 public enum ClientMode
 {
     Normal,
